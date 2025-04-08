e209c7918555ccc4649808fa70b14fe208364cdb
sachinrawat6 <logistics@qurvii.com>
2025-04-08 18:36:18 +0530

    flex box added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,20 +2,31 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-gray-100 px-4 mt-8  py-4 mx-auto flex flex-col gap-4 md:max-w-3xl rounded-md  w-[90vw] mb-4 text-gray-600'>
+    <div className='bg-gray-100 px-4 mt-8 py-8 mx-auto flex flex-col gap-4  rounded-md  w-[90vw] mb-4 text-gray-600 md:w-[90vw] md:flex-row justify-evenly                                                                                           '>
     <div>
-    <h2 className= "text-2xl" > Address </h2>
-        <p>Qurvii, B-149, 2nd Floor, Sector 6, Noida, 201301, </p>
+  
+
+        <h2 className= "text-2xl" > Customer Support Hours </h2>
+    <p>Monday to Saturday, 10 AM to 6 PM </p>
+    <p> Call us at: +91 97170 79325 </p>
+    <p>  
+    Our team is here to assist you with your queries and support requests. </p>
+
+
+
         
     </div>
 
     <div>
 
-    <h2 className= "text-2xl" > Customer Support Hours </h2>
-    <p>Monday to Saturday, 10 AM to 6 PM </p>
-    <p> Call us at: +91 97170 79325 </p>
-    <p>  
-    Our team is here to assist you with your queries and support requests. </p>
+
+    <h2 className= "text-2xl" > Address </h2>
+        <p>Qurvii, B-149, 2nd Floor, Sector 6, Noida, 201301, </p>
+
+
+
+
+
     </div>
 
     <div>
