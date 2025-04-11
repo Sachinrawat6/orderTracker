f71c47ec9907a5e772acb690e83ce10c52da2225
sachinrawat6 <logistics@qurvii.com>
2025-04-11 17:56:33 +0530

    truncate remove

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,12 +5,21 @@ const Footer = () => {
     <>
     <hr className="mt-6 text-gray-200" /> 
     <div className="bg-gray-10 px-4 mt-8 py-8 mx-auto flex flex-col gap-4  text-center rounded-md  w-[90vw] mb-4 text-gray-600 md:w-[90vw] md:flex-row justify-evenly ">
+     
+
+      <div>
+        <h2 className="md:text-2xl font-[500] text-black  "> Address </h2>
+        <p className="text-[12px] md:text-[14px] ">
+          Qurvii, B-149, 2nd Floor, Sector 6, Noida, 201301,{" "}
+        </p>
+      </div>
+
       <div>
         <h2 className="md:text-2xl font-[500] text-black  ">
           {" "}
         Qurvii  Customer Support Hours{" "}
         </h2>
-        <p className="text-[12px] md:text-[14px] truncate">
+        <p className="text-[12px] md:text-[14px] ">
           Our team is here to assist you with your queries and support requests.{" "}
         </p>
         <p className="text-[12px] md:text-[14px]">
@@ -21,13 +30,6 @@ const Footer = () => {
           Call us at:<b> +91 97170 79325{" "}</b>
         </p>
        
-      </div>
-
-      <div>
-        <h2 className="md:text-2xl font-[500] text-black  "> Address </h2>
-        <p className="text-[12px] md:text-[14px] truncate">
-          Qurvii, B-149, 2nd Floor, Sector 6, Noida, 201301,{" "}
-        </p>
       </div>
 
       <div>
